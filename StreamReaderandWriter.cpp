e5f07e8a5488524dee2a14ada1272fa65ae593b1
keisyaamalia <kkaysaamalia@gmail.com>
2026-06-01 11:02:30 +0700

    menunjuk ke sebuah file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -34,4 +34,8 @@ int main()
     
     //membuka file dalam mode membaca
     ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
 }
